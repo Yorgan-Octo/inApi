cd0d9f328156f4a02dbe095fc771e3893d6d7bd0
Artem Priadchenko <yorgan.info@gmail.com>
2024-10-14 15:18:29 +0300

    b4

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+import numpy as np
+import cv2
+import io
+import torch
+import os
+import uuid
+
 app = FastAPI()
+
+# Загрузка вашей обученной модели
+model_path = 'yolov5/runs/train/bcd/weights/best.pt'  # Путь к вашей обученной модели
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+try:
+    # Загрузка модели YOLOv5
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path=model_path, device=device)
+    print("Model loaded successfully")
+except Exception as e:
+    print(f"Error loading model: {e}")
+
 
 # Модель данных
 class Message(BaseModel):
